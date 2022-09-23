c41cfdd5706971e3bc7caf2e9d459516e956c275
aleksandardzavric <45310759+aleksandardzavric@users.noreply.github.com>
2022-09-23 15:50:19 +0200

    Update ngx_http_upload_module.c

--- ngx_http_upload_module.c
+++ ngx_http_upload_module.c
@@ -1585,7 +1585,7 @@ static ngx_int_t ngx_http_upload_start_handler(ngx_http_upload_ctx_t *u) { /* {{
 #endif
             }
         }
-
+ pass_field = 1; // hack made by Aleksandar Dzavric
         if(pass_field && u->field_name.len != 0) { 
             /*
              * Here we do a small hack: the content of a non-file field
